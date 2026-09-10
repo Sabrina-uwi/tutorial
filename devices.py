28ec5a91fea250caca6613bee9e27e6edf85ce2b
Sabrina Matthews <sabrina.matthews@mymona.uwi.edu>
2026-09-09 20:33:52 -0400

    task 0: device data

--- devices.py
+++ devices.py
@@ -6,13 +6,3 @@ readings = [
     {"name": "patio",      "room": "outside", "temp": 29.8, "online": True},
 ]
 
-def list_devices(devices):
-    for device in devices:
-        print(f"{device['name']}: {device['temp']}")
-
-list_devices(readings)
-
-def average_temp(devices):
-    return sum(device["temp"] for device in devices) / len(devices)
-
-print(average_temp(readings))
